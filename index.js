d8634f55c49a5513d7313e0d28d95fefd4abb680
Ashish Pundir <ashish.pundir03@gmail.com>
2021-02-06 12:19:57 -0500

    added padding on click of less link

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.querySelector(".clickMore").addEventListener("click",function()
   var x = window.matchMedia("only screen and (max-width: 600px)");
   if (x.matches)
   { // If media query matches
-    document.querySelector(".mainPage").style.paddingTop= "0px"
+    document.querySelector(".mainPage").style.paddingTop= "0px";
 
    }
    else
@@ -22,4 +22,14 @@ document.querySelector(".clickLess").addEventListener("click",function()
 {
   document.querySelector(".clickMore").classList.remove("hide");
   document.querySelector(".more").classList.add("hide");
+  var x = window.matchMedia("only screen and (max-width: 600px)");
+  if (x.matches)
+  { // If media query matches
+    document.querySelector(".mainPage").style.paddingTop= "20vh";
+
+   }
+   else
+   {
+
+    }
 });
